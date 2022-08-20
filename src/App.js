bbf093fb29ab3db2bc3e164b1cdae41b2a649f0a
snehasis <snehasisdebbarman2016@gmail.com>
2022-08-20 23:51:31 +0530

    ksks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./index.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <div className="logo"> placeholder</div>
+        <div className="navItemContainer">
+          <div className="navItem">
+            Home
+            <div className="dot"> .</div>
+          </div>
+          <div className="navItem">
+            {" "}
+            Products
+            <div className="dot">.</div>
+          </div>
+          <div className="navItem">
+            {" "}
+            about
+            <div className="dot">.</div>
+          </div>
+          <div className="navItem">
+            {" "}
+            contacts
+            <div className="dot">.</div>
+          </div>
+        </div>
       </header>
+      <div className="sectionContainer">
+        <section className="section-1">
+          <div className="leftSection">
+            <div className="subHeading">Pursue</div>
+            <div className="heading">
+              Of Human <br /> Happiness One
+            </div>
+            <div className="description">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,
+              harum repellat corporis quis deleniti odit totam. Voluptate,
+              corporis. Voluptate, a!
+            </div>
+          </div>
+          <div className="rightSection">timeLine</div>
+        </section>
+        <section className="section-2">
+          <div className="tabs"></div>
+          <div className="tabs"></div>
+          <div className="tabs"></div>
+        </section>
+      </div>
     </div>
   );
 }
